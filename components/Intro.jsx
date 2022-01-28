e3993c29a071beb9a2bd4735d59f251e6b241bc5
codeddave <drizzzydave@gmail.com>
2022-01-28 13:23:58 +0100

    updated intro content

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -4,7 +4,18 @@ import styles from "../styles/Intro.module.css";
 const Intro = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.card}>first</div>
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          {" "}
+          <span className={styles.brand}>AVOCAD0</span> DIGITAL PRODUCT AGENCY
+        </h1>
+
+        <p className={styles.description}>
+          {" "}
+          Create live segments and target the right people for messsages based
+          on their behaviours
+        </p>
+      </div>
       <div className={styles.card}>Second</div>
     </div>
   );
