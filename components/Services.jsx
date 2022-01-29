1c192d0249f5b36642bb4e92b5851f1983a0529c
codeddave <drizzzydave@gmail.com>
2022-01-29 13:37:23 +0100

    building out Services section

---
+++ components/Services.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Services.module.css";
+
+const Services = () => {
+  return <div className={styles.container}>services</div>;
+};
+
+export default Services;
