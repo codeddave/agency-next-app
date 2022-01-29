3fb596f8f2c70468700b29c38f539326b8700f03
codeddave <drizzzydave@gmail.com>
2022-01-29 20:54:07 +0100

    updates to Services component

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Services.module.css";
@@ -8,8 +9,29 @@ const Services = ({ services }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>What We Can Do?</h1>
       {services.map((service) => (
-        <Link key={service.id} href={`/products/${service.name}`}>
-          {service.title}
+        <Link passHref key={service.id} href={`/products/${service.name}`}>
+          <div className={styles.service}>
+            <span className={styles.cat}>{service.title}</span>
+
+            <div className={styles.media}>
+              {service.video ? (
+                <video
+                  src={service.video}
+                  autoPlay
+                  loop
+                  className={styles.video}
+                />
+              ) : (
+                <Image
+                  src={`/img/${service.photo}`}
+                  width="100%"
+                  height="100%"
+                  layout="responsive"
+                  alt={service.name}
+                />
+              )}
+            </div>
+          </div>
         </Link>
       ))}
     </div>
