3f9f7419ec18e2e77d059cfd846e5eba66a9df90
codeddave <drizzzydave@gmail.com>
2022-01-30 17:10:15 +0100

    Testimonials component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Intro from "../components/Intro";
 import Services from "../components/Services";
+import Testimonials from "../components/Testimonials";
 import { data } from "../data";
 
 import styles from "../styles/Home.module.css";
@@ -16,7 +17,8 @@ export default function Home({ services }) {
       </Head>
       <Intro />
       <Services services={services} />
-      This is homepage
+
+      <Testimonials />
     </div>
   );
 }
