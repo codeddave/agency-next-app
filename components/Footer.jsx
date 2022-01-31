e7b73443d58ee8542f8371f0a103362ee3568dd7
codeddave <drizzzydave@gmail.com>
2022-01-31 12:26:06 +0100

    styling and content updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,23 @@ import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.container}>
-      <div>PRODEV</div>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p className={styles.logo}>PRODEV</p>
+        <div className={styles.second}>
+          <div>
+            <p>WORK WITH US</p>
+            <p className={styles.block}>&copy; 2022 Prodev Interactive</p>
+          </div>
+
+          <div className={styles.card}>
+            <p>12 Adam Street, USA</p>
+            <p>212 444 516</p>
+
+            <p>contact@lama.dev</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
