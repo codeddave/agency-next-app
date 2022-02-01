94054099444f7f7a03da70c53cd7b858dd467897
codeddave <drizzzydave@gmail.com>
2022-02-01 06:59:56 +0100

    updates

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -1,8 +1,26 @@
+import Image from "next/image";
 import React from "react";
 import { data } from "../../data";
 import styles from "../../styles/Product.module.css";
-const Product = () => {
-  return <div className={styles.container}></div>;
+const Product = ({ product }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.largeCard}>
+        {product.images((img) => (
+          <div key={img.id} className={styles.imageContainer}>
+            <Image
+              src={img.url}
+              width="100%"
+              height="100%"
+              objectFit="cover"
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+      <div className={styles.smallCard}></div>
+    </div>
+  );
 };
 
 export default Product;
