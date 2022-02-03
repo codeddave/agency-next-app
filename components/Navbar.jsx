84aa6c4c75678e336d727cd15fde436a0eb22038
codeddave <drizzzydave@gmail.com>
2022-02-03 22:10:50 +0100

    created Hamburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
+
+import { Sling as Hamburger } from "hamburger-react";
+import { useState } from "react";
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className={styles.container}>
       <Link href="/">PRODEV</Link>
@@ -21,6 +26,29 @@ const Navbar = () => {
           <Link href="/products/contact  "> CONTACT</Link>
         </li>
       </ul>
+      <div className={styles.hamburger}>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+
+        {isOpen ? (
+          <ul className={styles.menu}>
+            <li className={styles.menuItem} onClick={() => setOpen(false)}>
+              <Link href="/products/design"> DESIGN</Link>
+            </li>
+            <li className={styles.menuItem} onClick={() => setOpen(false)}>
+              <Link href="/products/development">DEVELOPMENT</Link>
+            </li>
+            <li className={styles.menuItem} onClick={() => setOpen(false)}>
+              <Link href="/products/production"> PRODUCTION</Link>
+            </li>
+            <li className={styles.menuItem} onClick={() => setOpen(false)}>
+              <Link href="/products/photography"> PHOTOGRAPHY</Link>
+            </li>
+            <li className={styles.menuItem} onClick={() => setOpen(false)}>
+              <Link href="/products/contact  "> CONTACT</Link>
+            </li>
+          </ul>
+        ) : null}
+      </div>
     </div>
   );
 };
